6c0d460c2f345cf57f17d40c48732ae7b8d08b5a
patrickwide <patrickwide254@gmail.com>
2023-04-28 20:55:38 +0300

    Done work

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,7 +7,7 @@ function Counter() {
     setCount(count + 1);
   }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  return <button onClick={increment}>{count}</button>;
 }
 
 export default Counter;
